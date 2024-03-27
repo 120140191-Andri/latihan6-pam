d01efd1e68d419fed9500b2d1135e24fa84b93ba
Andri450 <andrisetiawan010101@gmail.com>
2024-03-27 15:24:22 +0700

    ryc

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,16 +1,30 @@
-package com.example.myapplication
-
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.myapplication.R
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val users = mutableListOf<User>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val json = resources.openRawResource(R.raw.users)
+            .bufferedReader().use { it.readText() }
+
+        val type = object : TypeToken<List<User>>() {}.type
+        val userList: List<User> = Gson().fromJson(json, type)
+
+        users.addAll(userList)
+
+//        val recyclerView = findViewById(R.id.ryc)
+//
+//        recyclerView.layoutManager = LinearLayoutManager(this)
+//        recyclerView.adapter = UserAdapter(users)
     }
 }
